6d2d16bf57eb1fc6019e80c0b477b00791611694
Nika <j.kunow@outlook.de>
2021-12-07 15:03:18 +0100

    regenerate frontend to change name and add scss

--- frontend/plant-pod-companion/src/app/app.component.ts
+++ frontend/plant-pod-companion/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'plant-pod-companion-app';
+  title = 'plant-pod-companion';
 }
