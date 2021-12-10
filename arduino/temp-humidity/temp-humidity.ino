a0ee331f5a309c7d8e4e4705471c311c1eecc0c8
Nika <j.kunow@outlook.de>
2021-12-10 23:23:00 +0100

    cleanup

--- arduino/temp-humidity/temp-humidity.ino
+++ arduino/temp-humidity/temp-humidity.ino
@@ -1,37 +1,40 @@
 #include "DHT.h"
 
-#define DHTPIN 2     // Pin 2 used by temp & humidity sensor
-#define DHTTYPE DHT11   // Defines type of sensor
+#define DHTPIN 2          // Pin 2 used by temp & humidity sensor
+#define DHTTYPE DHT11     // Defines type of sensor
 DHT dht(DHTPIN, DHTTYPE); // combines sensor data
 
 //startup arduino
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   Serial.println(F("Serial started"));
 
   dht.begin();
 }
 
-void loop() {
-  delay(5000); //delay between loop (1 sec = 1000)
-  float humidity = dht.readHumidity(); // reads humidity
-  float temp = dht.readTemperature();   // Read temperature as Celsius
+void loop()
+{
+  delay(5000);                               //delay between loop (1 sec = 1000)
+  float humidity = dht.readHumidity();       // reads humidity
+  float temperature = dht.readTemperature(); // Read temperature as Celsius
 
   // Checks if any sensor has failed
-  if (isnan(humidity) || isnan(temp)) {
-    Serial.println(F("Temp/humidity sensor failed!"));
+  if (isnan(humidity) || isnan(temperature))
+  {
+    Serial.println(F("Temperature or humidity sensor failed!"));
     return;
   }
 
   // Compute heat index in Celsius
-  float heatindex = dht.computeHeatIndex(temp, humidity, false);
+  float heatIndex = dht.computeHeatIndex(temperature, humidity, false);
 
   Serial.print(F("Humidity: "));
   Serial.print(humidity);
   Serial.print(F("%  Temperature: "));
-  Serial.print(temp);
+  Serial.print(temperature);
   Serial.print(F("°C "));
   Serial.print(F(" Heat index: "));
-  Serial.print(heatindex);
+  Serial.print(heatIndex);
   Serial.println(F("°C "));
 }
