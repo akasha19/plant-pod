a859fdc71ce5cb4fb81735a685da4be26da0a854
Nika <j.kunow@outlook.de>
2021-12-13 16:03:27 +0100

    cleanup, refactoring, add todos

---
+++ frontend/plant-pod-companion/src/app/plantpedia-page/plant.ts
@@ -0,0 +1,14 @@
+export interface Plant {
+    id: number;
+    longName: string;
+    shortName: string;
+    description: string;
+    minTemperature: number;
+    maxTemperature: number;
+    minph: number;
+    maxph: number;
+    care: string;
+    minHumidity: number;
+    maxHumidity: number;
+    imageSource: string;
+}
