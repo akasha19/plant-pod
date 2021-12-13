77b49b0cc43d297500aceb79505816fcd9ea24b9
Nika <j.kunow@outlook.de>
2021-12-13 01:37:23 +0100

    add dbContext and basic plant model

---
+++ backend/PlantPodService/model/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace PlantPodService.model
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<PlantEntity> Plants { get; set; }
+
+        public string DbPath { get; }
+
+        public DatabaseContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            //Todo: store connection string in appsettings.json
+            DbPath = System.IO.Path.Join(path, "database.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
---
+++ backend/PlantPodService/model/PlantEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PlantPodService.model
+{
+    public class PlantEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
