98e7606e107fa2a6bc9b78cd005fcd03db1561ad
Nika <j.kunow@outlook.de>
2021-12-15 22:59:05 +0100

    add database migration

---
+++ backend/PlantPodService/Migrations/20211215215802_add_plant_properties.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlantPodService.Migrations
+{
+    public partial class add_plant_properties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Plants",
+                newName: "ShortName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Care",
+                table: "Plants",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Plants",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Plants",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LongName",
+                table: "Plants",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "MaxHumidity",
+                table: "Plants",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "MaxTemperature",
+                table: "Plants",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Maxph",
+                table: "Plants",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "MinHumidity",
+                table: "Plants",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "MinTemperature",
+                table: "Plants",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Minph",
+                table: "Plants",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Moisture",
+                table: "Plants",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Care",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "LongName",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "MaxHumidity",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "MaxTemperature",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "Maxph",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "MinHumidity",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "MinTemperature",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "Minph",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "Moisture",
+                table: "Plants");
+
+            migrationBuilder.RenameColumn(
+                name: "ShortName",
+                table: "Plants",
+                newName: "Name");
+        }
+    }
+}
