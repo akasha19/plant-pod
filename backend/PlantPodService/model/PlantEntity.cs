cc1ff89466761a9b1a801438c2cf15a77e0e5fe0
Nika <j.kunow@outlook.de>
2021-12-15 22:58:54 +0100

    add properties to plant entity

--- backend/PlantPodService/model/PlantEntity.cs
+++ backend/PlantPodService/model/PlantEntity.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PlantPodService.Model
@@ -8,8 +7,19 @@ namespace PlantPodService.Model
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public Guid Id { get; set; }
+        public string Id { get; set; }
 
-        public string Name { get; set; }
+        public string LongName { get; set; }
+        public string ShortName { get; set; }
+        public string Description { get; set; }
+        public string Care { get; set; }
+        public decimal MinTemperature { get; set; }
+        public decimal MaxTemperature { get; set; }
+        public decimal Minph { get; set; }
+        public decimal Maxph { get; set; }
+        public decimal MinHumidity { get; set; }
+        public decimal MaxHumidity { get; set; }
+        public decimal Moisture { get; set; }
+        public string Image { get; set; }
     }
 }
