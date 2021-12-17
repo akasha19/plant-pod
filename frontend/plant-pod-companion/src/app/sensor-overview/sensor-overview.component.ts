7655828b031ae88f2aa59a69500fbd706fa6d1ef
djaantjie <pip.pannekoek@gmail.com>
2021-12-17 15:40:40 +0100

    add sensor overview component

---
+++ frontend/plant-pod-companion/src/app/sensor-overview/sensor-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sensor-overview',
+  templateUrl: './sensor-overview.component.html',
+  styleUrls: ['./sensor-overview.component.scss']
+})
+export class SensorOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
