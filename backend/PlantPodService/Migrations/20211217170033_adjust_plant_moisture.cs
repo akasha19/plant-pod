f4560535dc9a2adebfe9c6108330278e7670e5e3
Nika <j.kunow@outlook.de>
2021-12-17 18:01:09 +0100

    add migration

---
+++ backend/PlantPodService/Migrations/20211217170033_adjust_plant_moisture.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlantPodService.Migrations
+{
+    public partial class adjust_plant_moisture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Moisture",
+                table: "Plants",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Moisture",
+                table: "Plants",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+    }
+}
