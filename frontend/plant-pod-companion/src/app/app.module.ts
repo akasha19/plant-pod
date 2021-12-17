08bbc7d572fea49ac7c2bac3b98a45bfd02cb97a
djaantjie <pip.pannekoek@gmail.com>
2021-12-17 15:55:36 +0100

    bugfix

--- frontend/plant-pod-companion/src/app/app.module.ts
+++ frontend/plant-pod-companion/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SensorOverviewComponent } from './sensor-overview/sensor-overview.component';
+import { MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SensorOverviewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
