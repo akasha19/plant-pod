65193e0b5f32c9d2bb4865db1477f56ff0c36bf9
Nika <j.kunow@outlook.de>
2021-12-17 19:02:30 +0100

    add data annotations

--- backend/PlantPodService/model/PlantEntity.cs
+++ backend/PlantPodService/model/PlantEntity.cs
@@ -10,17 +10,38 @@ namespace PlantPodService.Model
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public Guid Id { get; set; }
 
+        [MaxLength(64)]
         public string LongName { get; set; }
+        [MaxLength(64)]
         public string ShortName { get; set; }
+
+        [MaxLength(2048)]
         public string Description { get; set; }
+
+        [MaxLength(2048)]
         public string Care { get; set; }
+
+        [Column(TypeName = "decimal(16,2)")]
         public decimal MinTemperature { get; set; }
+
+        [Column(TypeName = "decimal(16,2)")]
         public decimal MaxTemperature { get; set; }
+
+        [Column(TypeName = "decimal(16,2)")]
         public decimal Minph { get; set; }
+
+        [Column(TypeName = "decimal(16,2)")]
         public decimal Maxph { get; set; }
+
+        [Column(TypeName = "decimal(16,2)")]
         public decimal MinHumidity { get; set; }
+
+        [Column(TypeName = "decimal(16,2)")]
         public decimal MaxHumidity { get; set; }
+        
         public Moisture Moisture { get; set; }
+
+        [MaxLength(64)]
         public string Image { get; set; }
     }
 }
