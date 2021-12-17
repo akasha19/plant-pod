541f9c6bcac7a56d311ba120a72dc772113e0f7a
Nika <j.kunow@outlook.de>
2021-12-17 18:14:12 +0100

    add migration

--- backend/PlantPodService/Migrations/20211217171337_make_plant_id_guid.Designer.cs
+++ backend/PlantPodService/Migrations/20211217171337_make_plant_id_guid.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PlantPodService.Model;
 
 namespace PlantPodService.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20211217171337_make_plant_id_guid")]
+    partial class make_plant_id_guid
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/PlantPodService/Migrations/20211217171337_make_plant_id_guid.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlantPodService.Migrations
+{
+    public partial class make_plant_id_guid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
