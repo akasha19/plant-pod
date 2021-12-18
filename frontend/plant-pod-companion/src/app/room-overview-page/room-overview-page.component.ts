a049be1aafeebaeac9eba308205cb19983d5f05c
Nika <j.kunow@outlook.de>
2021-12-18 13:31:11 +0100

    renaming fixes

--- frontend/plant-pod-companion/src/app/room-overview-page/room-overview-page.component.ts
+++ frontend/plant-pod-companion/src/app/room-overview-page/room-overview-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RoomData } from '../types/RoomData';
+import { Room } from '../types/Room';
 @Component({
   selector: 'app-room-overview-page',
   templateUrl: './room-overview-page.component.html',
@@ -8,7 +8,7 @@ import { RoomData } from '../types/RoomData';
 
 export class RoomOverviewPageComponent implements OnInit {
 
-  Room: RoomData[] | undefined;
+  Room: Room[] | undefined;
 
   readonly maxTemperatureWarn = "(Maximum Temperature) The temperature of this room it too warm which can cause fatigue and headaches, please open the window for some fresh air.";
   readonly minTemperatureWarn = "(Minimum Temperature) The temperature of this room it too cold which can cause discomfort and a higher chance of sickness, please turn on the heater to heat up the room.";
@@ -20,7 +20,7 @@ export class RoomOverviewPageComponent implements OnInit {
         sensorId: "1",
         name: "Room 1 | presentation room",
         description: "presentation room for 100 people",
-        facilities: ['Airconditioning','heating','smartprojector','smartplants'],
+        facilities: ['Airconditioning', 'heating', 'smartprojector', 'smartplants'],
         imageSource: "assets/img/room_1.jpg"
       },
       {
@@ -28,7 +28,7 @@ export class RoomOverviewPageComponent implements OnInit {
         sensorId: "2",
         name: "Room 2 | meeting room",
         description: "meeting room for 8 people",
-        facilities: ['Airconditioning','heating','smartboard', 'smartplants'],
+        facilities: ['Airconditioning', 'heating', 'smartboard', 'smartplants'],
         imageSource: "assets/img/room_2.jpg"
       },
 
