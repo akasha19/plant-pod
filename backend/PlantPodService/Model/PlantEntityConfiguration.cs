a6139e9172990df231ef150352207aa21ff655f5
Nika <j.kunow@outlook.de>
2021-12-20 23:05:00 +0100

    add Plant Entity configuration

---
+++ backend/PlantPodService/Model/PlantEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace PlantPodService.Model
+{
+    public class PlantEntityConfiguration : IEntityTypeConfiguration<PlantEntity>
+    {
+        public void Configure(EntityTypeBuilder<PlantEntity> builder)
+        {
+            builder.HasData(new []
+            {
+                new PlantEntity { Id = Guid.NewGuid() },
+                new PlantEntity { Id = Guid.NewGuid() }
+            });
+        }
+    }
+}
