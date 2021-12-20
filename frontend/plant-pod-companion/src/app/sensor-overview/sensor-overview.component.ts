07e13ddcdc8c580cc7436ea7c72709e70b6b8711
djaantjie <pip.pannekoek@gmail.com>
2021-12-20 16:26:24 +0100

    added room sensor overview component

--- frontend/plant-pod-companion/src/app/sensor-overview/sensor-overview.component.ts
+++ frontend/plant-pod-companion/src/app/sensor-overview/sensor-overview.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Sensor } from '../types/Sensor';
 
 @Component({
   selector: 'sensor-overview',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SensorOverviewComponent implements OnInit {
 
+  @Input()
+  id: string | undefined;
+
+  sensor: Sensor | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.sensor = {
+      id: this.id ?? "",
+      humidity: 2,
+      ph: 5,
+      temperature: 8,
+      moisture: 4,
+    }
+
   }
+
 
 }
