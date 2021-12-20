b1fbf9c0007ca538d484fbf9c5f0900001c96d6a
boluijten3 <b.c.luijten@st.hanze.nl>
2021-12-20 16:33:10 +0100

    asked changes in pr room-overview-page

--- frontend/plant-pod-companion/src/app/room-overview-page/room-overview-page.component.ts
+++ frontend/plant-pod-companion/src/app/room-overview-page/room-overview-page.component.ts
@@ -8,17 +8,17 @@ import { Room } from '../types/Room';
 
 export class RoomOverviewPageComponent implements OnInit {
 
-  Room: Room[] | undefined;
+  Rooms: Room[] | undefined;
 
   readonly maxTemperatureWarn = "(Maximum Temperature) The temperature of this room it too warm which can cause fatigue and headaches, please open the window for some fresh air.";
   readonly minTemperatureWarn = "(Minimum Temperature) The temperature of this room it too cold which can cause discomfort and a higher chance of sickness, please turn on the heater to heat up the room.";
 
   ngOnInit(): void {
-    this.Room = [
+    this.Rooms = [
       {
         id: "1",
         sensorId: "1",
-        name: "Room 1 | presentation room",
+        name: "Presentation room",
         description: "presentation room for 100 people",
         facilities: ['Airconditioning', 'heating', 'smartprojector', 'smartplants'],
         imageSource: "assets/img/room_1.jpg"
@@ -26,7 +26,7 @@ export class RoomOverviewPageComponent implements OnInit {
       {
         id: "2",
         sensorId: "2",
-        name: "Room 2 | meeting room",
+        name: "Meeting room",
         description: "meeting room for 8 people",
         facilities: ['Airconditioning', 'heating', 'smartboard', 'smartplants'],
         imageSource: "assets/img/room_2.jpg"
