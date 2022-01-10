b4194868f56847b5c0d38194e035a4f02beaad89
boluijten3 <b.c.luijten@st.hanze.nl>
2022-01-10 17:45:39 +0100

    rename labels on Tooltips

--- frontend/plant-pod-companion/src/app/room-overview-page/room-overview-page.component.ts
+++ frontend/plant-pod-companion/src/app/room-overview-page/room-overview-page.component.ts
@@ -10,8 +10,8 @@ export class RoomOverviewPageComponent implements OnInit {
 
   rooms: Room[] | undefined;
 
-  readonly maxTemperatureWarn = "(Maximum Temperature) The temperature of this room it too warm which can cause fatigue and headaches, please open the window for some fresh air.";
-  readonly minTemperatureWarn = "(Minimum Temperature) The temperature of this room it too cold which can cause discomfort and a higher chance of sickness, please turn on the heater to heat up the room.";
+  readonly maxTooltip = "(Maximum Temperature) The temperature of this room it too warm which can cause fatigue and headaches, please open the window for some fresh air.";
+  readonly minTooltip = "(Minimum Temperature) The temperature of this room it too cold which can cause discomfort and a higher chance of sickness, please turn on the heater to heat up the room.";
 
   ngOnInit(): void {
     this.rooms = [
