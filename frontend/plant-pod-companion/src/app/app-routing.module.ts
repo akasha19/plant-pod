621036b3fd3f0d58962a947d05d3cb6b439cf043
Nika <j.kunow@outlook.de>
2022-01-11 11:19:03 +0100

    refactor routing

--- frontend/plant-pod-companion/src/app/app-routing.module.ts
+++ frontend/plant-pod-companion/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlantpediaPageComponent } from './plantpedia-page/plantpedia-page.component';
+import { RoomDetailsPageComponent } from './room-details-page/room-details-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'plantpedia',
+    component: PlantpediaPageComponent
+  },
+  {
+    path: 'room-details',
+    component: RoomDetailsPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
