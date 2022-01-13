03c04ae02f385fc50275728bc00aaee948274a0e
boluijten3 <b.c.luijten@st.hanze.nl>
2022-01-13 12:28:41 +0100

    routing and padding

--- frontend/plant-pod-companion/src/app/app-routing.module.ts
+++ frontend/plant-pod-companion/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlantpediaPageComponent } from './plantpedia-page/plantpedia-page.component';
 import { RoomOverviewPageComponent } from './room-overview-page/room-overview-page.component';
+import { PlantDetailsPageComponent } from './plant-details-page/plant-details-page.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'room-overview',
     component: RoomOverviewPageComponent
+  },
+  {
+    path: 'plant-details-page',
+    component: PlantDetailsPageComponent
   }
 ];
 
