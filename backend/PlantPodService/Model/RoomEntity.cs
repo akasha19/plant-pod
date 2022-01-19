a0815373e20d83c2385b35c7d9e2a0e58fc0fada
Nika <j.kunow@outlook.de>
2022-01-19 13:39:33 +0100

    add data annotations

--- backend/PlantPodService/Model/RoomEntity.cs
+++ backend/PlantPodService/Model/RoomEntity.cs
@@ -1,19 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PlantPodService.Model
 {
-    public sealed class RoomEntity
+    public class RoomEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public Guid Id { get; set; }
 
         public Guid SensorId { get; set; }
 
+        [MaxLength(64)]
         public string Name { get; set; }
-        
+
+        [MaxLength(2048)]
         public string Description { get; set; }
         
         public string[] Facilities { get; set; }
-        
+
+        [MaxLength(64)]
         public string ImageSource { get; set; }
     }
 }
