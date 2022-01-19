a37dcba0560e5f550da31c8254175e4c7bcd0111
Nika <j.kunow@outlook.de>
2022-01-19 14:38:13 +0100

    add tests for room service

--- backend/PlantPodService/Services/Persistence/RoomService.cs
+++ backend/PlantPodService/Services/Persistence/RoomService.cs
@@ -7,16 +7,23 @@ namespace PlantPodService.Services.Persistence
 {
     public interface IRoomService
     {
-        public IEnumerable<RoomEntity> GetRooms();
+        public IEnumerable<RoomEntity> GetAllRooms();
 
-        public RoomEntity GetRoomById();
+        public RoomEntity GetRoomById(Guid id);
 
         public ImmutableArray<Guid> GetSensorIds();
     }
 
     public sealed class RoomService : IRoomService
     {
-        public RoomEntity GetRoomById()
+        private readonly PlantPodServiceDbContext _dbContext;
+
+        public RoomService(PlantPodServiceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public RoomEntity GetRoomById(Guid id)
         {
             throw new NotImplementedException();
         }
@@ -27,7 +34,7 @@ namespace PlantPodService.Services.Persistence
             return new [] { Guid.Parse("196db225-e5ef-4636-b967-c214a0ddb73f") }.ToImmutableArray();
         }
 
-        public IEnumerable<RoomEntity> GetRooms()
+        public IEnumerable<RoomEntity> GetAllRooms()
         {
             throw new NotImplementedException();
         }
---
+++ backend/PlantPodServiceTests/Factories/RoomFactory.cs
@@ -0,0 +1,35 @@
+﻿using PlantPodService.Model;
+using PlantPodService.ViewModel;
+using System;
+
+namespace PlantPodServiceTests.Factories
+{
+    internal static class RoomFactory
+    {
+        public static RoomEntity RoomEntity() => RoomEntity(Guid.NewGuid(), Guid.NewGuid());
+        
+        public static RoomEntity RoomEntity(Guid id) => RoomEntity(id, Guid.NewGuid());
+
+        public static RoomEntity RoomEntity(Guid id, Guid sensorId) => new RoomEntity
+        {
+            Id = id,
+            Name = "some Room",
+            Description = "Description for some room",
+            Facilities = new[] { "a facility", "another facility", "and a third" },
+            ImageSource = "some/path/to/image.jpg",
+            SensorId = sensorId
+        };
+
+        public static Room Room(Guid id) => Room(id, Guid.NewGuid());
+
+        public static Room Room(Guid id, Guid sensorId) => new Room
+        {
+            Id = id,
+            Name = "some Room",
+            Description = "Description for some room",
+            Facilities = new[] {"a facility", "another facility", "and a third"},
+            ImageSource = "some/path/to/image.jpg",
+            SensorId = sensorId
+        };
+    }
+}
