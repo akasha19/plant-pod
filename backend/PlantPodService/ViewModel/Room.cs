86c4ca740a5b0cec95c7255cf8972c564664de3e
Nika <j.kunow@outlook.de>
2022-01-19 14:19:01 +0100

    add room view model

---
+++ backend/PlantPodService/ViewModel/Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PlantPodService.ViewModel
+{
+    public class Room
+    {
+        public Guid Id { get; set; }
+        public Guid SensorId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string[] Facilities { get; set; }
+        public string ImageSource { get; set; }
+    }
+}
